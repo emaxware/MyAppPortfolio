05b2a2af91d81af9512a153a36060cf06862e21f
emaxware <github@emaxware.com>
2015-06-12 06:27:33 -0400

    Cleaned up warnings.

---
+++ app/src/main/java/com/emaxware/udacity/myappportfolio/MainActivity.java
@@ -0,0 +1,61 @@
+package com.emaxware.udacity.myappportfolio;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.*;
+import android.content.*;
+
+import static java.lang.String.format;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CharSequence text = v.getContentDescription();
+
+                Toast.makeText(
+                    getApplicationContext()
+                    , String.format("This button will launch my %s app!", text)
+                    , Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        findViewById(R.id.btn1).setOnClickListener(listener);
+        findViewById(R.id.btn2).setOnClickListener(listener);
+        findViewById(R.id.btn3).setOnClickListener(listener);
+        findViewById(R.id.btn4).setOnClickListener(listener);
+        findViewById(R.id.btn5).setOnClickListener(listener);
+        findViewById(R.id.btn6).setOnClickListener(listener);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
